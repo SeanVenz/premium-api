0c50c66bfdb8239e030641ab01a979c22c55298d
shan <quijano.seanvenz@gmail.com>
2025-07-29 11:22:16 +0800

    remove comment and unecessary test file

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,10 +2,9 @@ import axios from 'axios';
 
 const API_BASE_URL = import.meta.env.VITE_API_URL;;
 
-// Create axios instance with default config
 const api = axios.create({
   baseURL: API_BASE_URL,
-  withCredentials: true, // Important for sending cookies
+  withCredentials: true,
   headers: {
     'Content-Type': 'application/json'
   }
